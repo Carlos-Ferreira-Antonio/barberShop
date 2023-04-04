2df1a9904b7c65d0df33c8510987fd69c6a41b7e
Carlos Antonio <carlosantonio_ferreirapaula@outlook.com>
2023-04-04 15:55:44 -0300

    Aplicação de herança em Models

--- src/Application/MainLogin.java
+++ src/Application/MainLogin.java
@@ -1,24 +1,27 @@
 package Application;
 
+import Model.Agendamento;
 import Model.Cliente;
 import Model.Servico;
 import Model.Usuario;
+import java.text.ParseException;
 
 public class MainLogin {
     
-    public static void main(String[]args){
+    public static void main(String[]args) throws ParseException{
     
-        /*
         String nome = "Carlos";
         System.out.println("Cliente: " + nome);
         Servico svc = new Servico(1, "barba", 30);
         System.out.println("Serviço: "+ svc.getDescricao() + " \nValor: " + svc.getValor());
-        */
-     
-        Usuario usu = new Usuario(1, "Jhon", "barbado");
-        System.out.println(usu);
+  
+        Usuario usu = new Usuario(1, "Jhonn", "barbado");
+        System.out.println(usu.getNome());
         
-        Cliente cli = new Cliente(1, "Alan", 'M', "063925887484", "Rua 123");
-         System.out.println(cli);
+        Cliente cli = new Cliente(1, "Carlos", "Rua123", "77470555");
+         System.out.println(cli.getNome());
+         
+        Agendamento agd = new Agendamento(1, cli, svc, 30, "05/04/2023 08:00");
+        System.out.println(agd.getCliente().getNome());
     }
 }
--- src/Model/Cliente.java
+++ src/Model/Cliente.java
@@ -1,97 +1,24 @@
 package Model;
 
-import java.util.Date;
+import java.text.ParseException;
 
-public class Cliente {
+public class Cliente extends Pessoa {
     
-    private int id;
-    private String nome;
-    private char sexo;
-    private Date dataNascimento;
-    private String telefone;
-    private String email;
-    private String rg;
     private String endereco;
     private String cep;
 
-    public Cliente(int id, String nome, char sexo, String telefone, String endereco) {
-        this.id = id;
-        this.nome = nome;
-        this.sexo = sexo;
-        this.telefone = telefone;
-        this.endereco = endereco;
-    }
-            
-    public Cliente(int id, String nome, char sexo, Date dataNascimento, String telefone, String email, String rg, String endereco, String cep) {
-        this.id = id;
-        this.nome = nome;
-        this.sexo = sexo;
-        this.dataNascimento = dataNascimento;
-        this.telefone = telefone;
-        this.email = email;
-        this.rg = rg;
+    public Cliente(int id, String nome, char sexo, String dataNascimento, String telefone, String email, String rg, String endereco, String cep) throws ParseException {
+        super(id, nome, sexo, dataNascimento, telefone, email, rg);
         this.endereco = endereco;
         this.cep = cep;
     }
 
-    public int getId() {
-        return id;
+    public Cliente(int id, String nome, String endereco, String cep) {
+        super(id, nome);
+        this.endereco = endereco;
+        this.cep = cep;
     }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     
-    
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public char getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(char sexo) {
-        this.sexo = sexo;
-    }
-
-    public Date getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(Date dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRg() {
-        return rg;
-    }
-
-    public void setRg(String rg) {
-        this.rg = rg;
-    }
-
     public String getEndereco() {
         return endereco;
     }
@@ -107,4 +34,5 @@ public class Cliente {
     public void setCep(String cep) {
         this.cep = cep;
     }
+
 }
